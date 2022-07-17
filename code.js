5f124a891a6e73dde078c35826b2f1139c451260
Mantelis01 <mantas.zukovskis@gmail.com>
2022-07-18 01:18:23 +0300

    adjustments

--- code.js
+++ code.js
@@ -2,33 +2,63 @@ function clearAll(){
     document.getElementById("result").value = "";
 }
 
-function addNumber(value){
-    document.getElementById("result").value += value;
-}
-
-function calculate(){
-    var p=document.getElementById("result").value;
-    var res=eval(p);
-    document.getElementById("result").value=res;
-}
-
 function clearOne(){
     var p=document.getElementById("result").value;
     document.getElementById("result").value=p.substring(0,p.length-1);
 }
 
+function addNumber(value){
+    document.getElementById("result").value += value; 
+}
+
+
 function addOperator(value){
     document.getElementById("result").value += value;
 }
-/*
-function addFunction(num1,num2){
-    if(num1==""){
-        num1=0;
+
+function add(num1,num2){
+    if(num1===0){
+        return 0;
     }
-    if(num2==""){
-        num2=0;
+    else if(num2===0){
+        return 0;
     }
-    var res=num1+num2;
-    return res;
+    return num1+num2;
 }
-*/
+
+function sub(num1,num2){
+    if(num1===0){
+        return 0;
+    }
+    else if(num2===0){
+        return 0;
+    }
+    return num1-num2;
+}
+
+function mul(num1,num2){
+    if(num1===0){
+        return 0;
+    }
+    else if(num2===0){
+        return 0;
+    }
+    return num1*num2;
+}
+
+function div(num1,num2){
+    if(num1===0){
+        return 0;
+    }
+    else if(num2===0){
+        return 0;
+    }
+    return num1/num2;
+}
+
+const mathFunc = {
+    '+': add,
+    '-': sub,
+    '*': mul,
+    '/': div,
+}
